cc4ec166acbf5b57416a6709534528cc16f2ce0a
SDI-sodya <saienko.dan06sdi@gmail.com>
2024-11-04 23:11:21 +0200

    Clicks counter moving to functional.js

--- assets/js/functional.js
+++ assets/js/functional.js
@@ -9,6 +9,21 @@ export function disabledFightButton(numButt) {
   buttons[numButt].disabled = true;
 }
 
-export function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+export function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Підрахування кліків (із замиканням)
+export const createButtonClickCounter = (maxClicks, numAtackButt) => {
+  let clickCount = 0;
+
+  return () => {
+    if (clickCount < maxClicks - 1) {
+      clickCount++;
+      console.log(`Button clicked ${clickCount} times. Remaining: ${maxClicks - clickCount}`);
+    } else {
+      console.log('Button has reached the maximum number of clicks.');
+      disabledFightButton(numAtackButt);
+    }
+  };
 }
