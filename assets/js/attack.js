8c56700c2b5704da435a9ca5ee3d3238e5f756c1
SDI-sodya <saienko.dan06sdi@gmail.com>
2024-11-04 23:11:29 +0200

    Make buttons and add damage rendering to fight buttons

--- assets/js/attack.js
+++ assets/js/attack.js
@@ -1,34 +1,39 @@
-import { renderLog, logBattle } from "./logs.js";
-import { disabledFightButton } from "./functional.js";
+import { renderLog, logBattle, logDefeat  } from "./logs.js";
+import { getRandomInt } from "./functional.js"
 
-function logDefeat(name = '') {
-  disabledFightButton(0);
-  disabledFightButton(1);
-  logBattle(name ? `${name} програв бій` : 'Обидва програли бій');
-}
-
-export function renderDamage(character, enemy) {
-  const { hp: characterHP, name: characterName } = character;
-  const { hp: enemyHP, name: enemyName } = enemy;
+export function renderDamage(character, enemy, damage) {
+  const characterHP = character.hp;
+  const enemyHP = enemy.hp;
 
   if(characterHP <= 0 || enemyHP <= 0) {
     if(characterHP <= 0) character.hp = 0;
     if(enemyHP <= 0) enemy.hp = 0;
-    character.renderHP();
-    enemy.renderHP();
 
-    if(characterHP <= 0 && enemyHP <= 0) {
-      logDefeat();
-    } else {
-      logDefeat(characterHP <= 0 ? characterName : enemyName);
-    }
+    renderHP(character, randomEnemyAttack(enemy), 0);
+    renderHP(enemy, damage, 1);
+
+    // if(characterHP <= 0 && enemyHP <= 0) {
+    //   logDefeat();
+    // } else {
+    //   logDefeat(characterHP <= 0 ? character.name : enemy.name);
+    // }
   } else {
-    character.renderHP();
-    enemy.renderHP();
-    renderLog(character, enemy);
+    renderHP(character, randomEnemyAttack(enemy), 0);
+    renderHP(enemy, damage, 1);
   }
 }
 
 export function pokemonAttack(character, enemy, damage = 0) {
   return character.attack(enemy, damage);
+}
+
+export function randomEnemyAttack(enemy) {
+  const attack = Math.random() < 0.5 ? enemy.attacks[0] : enemy.attacks[getRandomInt(1, 3)];
+  return getRandomInt(attack.minDamage, attack.maxDamage);
+}
+
+function renderHP(pokemon, damage, hpBar) {
+  pokemon.hp -= damage;
+  const characterHealth = document.getElementsByClassName('text')[hpBar];
+  characterHealth.innerText = `${pokemon.hp} / 100`;
 }
--- main.js
+++ main.js
@@ -1,39 +1,102 @@
-// Підключення логів з logs.js
-import { Pokemon } from "./assets/js/Pokemon.js";
-import { pokemonAttack } from "./assets/js/attack.js";
-import { init, disabledFightButton } from "./assets/js/functional.js";
+import { pokemons } from "./assets/js/pokemons.js"
+import { getRandomInt } from "./assets/js/functional.js"
+import { renderDamage } from "./assets/js/attack.js"
 
-// Підразування кліків (із замиканням)
-const createButtonClickCounter = (maxClicks, numAtackButt) => {
-  let clickCount = 0;
 
-  return () => {
-    if (clickCount < maxClicks - 1) {
-      clickCount++;
-      console.log(`Button clicked ${clickCount} times. Remaining: ${maxClicks - clickCount}`);
-    } else {
-      console.log('Button has reached the maximum number of clicks.');
-      disabledFightButton(numAtackButt);
-    }
-  };
+
+function createGame() {
+  const pokemonCharacter = pokemons[1];
+  let pokemonEnemy = pokemons[getRandomInt(1, pokemons.length - 1)];
+  renderAttackBtn(pokemonCharacter);
+  renderPokemon(pokemonCharacter);
+  // renderPokemon(pokemonEnemy);
 }
 
-let pokemonCharacter = new Pokemon('Pikachu', 100, 'electro', 'health-character', 'progressbar-character');
-let pokemonEnemy = new Pokemon('Charmander', 100, 'flame', 'health-enemy', 'progressbar-enemy');
-
-init(pokemonCharacter, pokemonEnemy);
 
 
-const thunderJoltCounter = createButtonClickCounter(7, 0); // Лічильник для кнопки Thunder Jolt
-const fireballCounter = createButtonClickCounter(9, 1); // Лічильник для кнопки Fireball
+function createAttackBtns(attackBtnsArr) {
+  const control = document.getElementById('control');
+
+  for(let i = 0; i < attackBtnsArr.length; i++) {
+    const newBtn = document.createElement('button');
+    newBtn.classList.add('button', 'fight-button');
+    newBtn.textContent = attackBtnsArr[i].name;
+    control.appendChild(newBtn);
+  }
+}
+
+
+
+function addFuncToBtns(character, enemy) {
+  const attackBtnsArr = document.getElementsByClassName('fight-button');
+  for(let i = 0; i < attackBtnsArr.length; i++) {
+    const attack = character.attacks[i];
+    attackBtnsArr[i].addEventListener('click', () => {
+      renderDamage(character, enemy, getRandomInt(attack.minDamage, attack.maxDamage));
+    });
+  }
+}
+
+
+
+function renderAttackBtn(character, enemy) {
+  const pokemonsAttacks = character.attacks;
+
+  createAttackBtns(pokemonsAttacks);
+  addFuncToBtns(character, enemy);
+}
+
+
+
+function renderPokemon(pokemon) {
+  
+}
+
+
+
+const pokemonCharacter = pokemons[0];
+let pokemonEnemy = pokemons[getRandomInt(1, pokemons.length - 1)];
 
 // Встановлюємо обробники подій
-document.getElementById('staticdamage').addEventListener('click', () => {
-    thunderJoltCounter();
-    pokemonAttack(pokemonCharacter, pokemonEnemy);
-});
+// document.getElementById('staticdamage').addEventListener('click', () => {
+//     thunderJoltCounter();
+//     pokemonAttack(pokemonCharacter, pokemonEnemy);
+// });
 
-document.querySelector('.fight-button:nth-child(2)').addEventListener('click', () => {
-    fireballCounter();
-    pokemonAttack(pokemonCharacter, pokemonEnemy, 10);
-});
+// document.querySelector('.fight-button:nth-child(2)').addEventListener('click', () => {
+//     fireballCounter();
+//     pokemonAttack(pokemonCharacter, pokemonEnemy, 10);
+// });
+
+
+/*
+  -1. Импортировать массив с покемонами import
+  2. Функция вывода имени покемона. renderName()
+    Долна принимать покемона и выводить его в определённый блок на странице
+  3 ???. Функция вывода типа покемона
+  4. Функция вывода ХП покемона rednerHP()
+    Должна принимать покемона и выводить его ХП в определённый блок на странице
+  5. Функция вывода картинки renderImg()
+    Должна принимать покемона и выводить его картинку в определённый блок на странице
+  -6_1. Функция созданяи кнопок с названиями атак createAttackBtn()
+  -6_2. Функция вывода массива кнопок renderAttackBtn()
+    Должна принимать покемона. Внутри 
+  -7. Функция атаки attack()
+    Должна принимать номер атаки у покемона.
+    При нажатии на кнопку рандомиться кол-во дамага (getRandomInt) и минусуется у противника.
+  -8. Присоединить функцию атаки к кнопке с указанными значениями минимального/максимального дамага
+  9. Присоединить функция счётчика количества ударов к кнопкам с указанными значениями максимального кол-ва кликов
+  10. 
+
+
+
+  Доп задания:
+  После того как вы победили врага, отрисуйте нового рандомного врага, чтобы продолжить драться...
+  Кол-во уждаров у вас ограничено, как и жизней, так что пользуйтесь ударами с умом...
+  Не все сильные удары самые эффективные...
+
+  Доп-доп задания:
+  Добавить классы .low и .critical.
+  В методе renderProgressbarHP() допишите условия, если жизней меньше 60 но больше 20, то добавляйте класс low, если меньше 20, то класс .critical
+
+*/
