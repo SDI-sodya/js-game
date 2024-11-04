aa08a301da6e74c8cbf869ad43c006cea3cf1102
SDI-sodya <saienko.dan06sdi@gmail.com>
2024-11-04 18:22:54 +0200

    Refactor render damage functions

--- assets/js/attack.js
+++ assets/js/attack.js
@@ -1,30 +1,27 @@
 import { renderLog, logBattle } from "../../logs.js";
 import { disabledFightButton } from "./functional.js";
 
+function logDefeat(name = '') {
+  disabledFightButton(0);
+  disabledFightButton(1);
+  logBattle(name ? `${name} програв бій` : 'Обидва програли бій');
+}
+
 export function renderDamage(character, enemy) {
   const { hp: characterHP, name: characterName } = character;
   const { hp: enemyHP, name: enemyName } = enemy;
 
-  if (characterHP <= 0 && characterHP < enemyHP) {
-    character.hp = 0;
-    character.renderHP();
-    disabledFightButton(0);
-    disabledFightButton(1);
-    logBattle(`${characterName} програв бій`);
-  } else if (enemyHP <= 0 && characterHP > enemyHP) {
-    enemy.hp = 0;
-    enemy.renderHP();
-    disabledFightButton(0);
-    disabledFightButton(1);
-    logBattle(`${enemyName} програв бій`);
-  } else if (characterHP <= 0 && enemyHP <= 0) {
-    character.hp = 0;
-    enemy.hp = 0;
+  if(characterHP <= 0 || enemyHP <= 0) {
+    if(characterHP <= 0) character.hp = 0;
+    if(enemyHP <= 0) enemy.hp = 0;
     character.renderHP();
     enemy.renderHP();
-    disabledFightButton(0);
-    disabledFightButton(1);
-    logBattle(`Обидва програли бій`);
+
+    if(characterHP <= 0 && enemyHP <= 0) {
+      logDefeat();
+    } else {
+      logDefeat(characterHP <= 0 ? characterName : enemyName);
+    }
   } else {
     character.renderHP();
     enemy.renderHP();
